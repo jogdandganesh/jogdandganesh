2c54a742d49a76cca72bc315e1b239b200fcc840
jogdandganesh <ganesh.198j@gmail.com>
2023-12-18 11:37:09 +0530

    update

---
+++ shopvista/src/main/java/com/shopvista/ShopvistaApplication.java
@@ -0,0 +1,15 @@
+package com.shopvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopvistaApplication.class, args);
+		System.out.println("hello");
+		System.out.println("CJC");
+	}
+
+}
